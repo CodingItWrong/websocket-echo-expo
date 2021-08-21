76a96550d91ed94ac3dc1169dc47dbc49b40324f
Josh Justice <me@codingitwrong.com>
2021-08-21 15:11:48 -0400

    Add simple message interface

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import {SafeAreaView, StatusBar, Text} from 'react-native';
+import React, {useState} from 'react';
+import {SafeAreaView, StatusBar, Text, TextInput} from 'react-native';
 
 export default function App() {
+  const [messages, setMessages] = useState([]);
+  const [newMessageText, setNewMessageText] = useState('');
+
+  function handleSend() {
+    setMessages([...messages, newMessageText]);
+    setNewMessageText('');
+  }
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <Text>Hello, React Native!</Text>
+        <TextInput
+          value={newMessageText}
+          onChangeText={setNewMessageText}
+          onSubmitEditing={handleSend}
+        />
+        {messages.map((message, index) => (
+          <Text key={index}>{message}</Text>
+        ))}
       </SafeAreaView>
     </>
   );
