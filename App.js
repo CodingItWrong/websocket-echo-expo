1849c82d883d820dae2861441a3dfeec7de28d50
Josh Justice <me@codingitwrong.com>
2021-08-22 06:17:46 -0400

    Get web socket working

--- App.js
+++ App.js
@@ -1,14 +1,48 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {SafeAreaView, StatusBar, Text, TextInput} from 'react-native';
+
+const wsUrl = 'ws://localhost:3000';
+
+let socket;
+
+function setUpWebSocket(onMessageReceived) {
+  if (!socket) {
+    socket = new WebSocket(wsUrl);
+    console.log('Attempting Connection...');
+
+    socket.onopen = () => {
+      console.log('Successfully Connected');
+    };
+
+    socket.onclose = event => {
+      console.log('Socket Closed Connection: ', event);
+      socket = null;
+    };
+
+    socket.onerror = error => {
+      console.error('Socket Error: ', error);
+    };
+  }
+
+  socket.onmessage = event => {
+    onMessageReceived(event.data);
+  };
+}
 
 export default function App() {
   const [messages, setMessages] = useState([]);
   const [newMessageText, setNewMessageText] = useState('');
 
   function handleSend() {
-    setMessages([...messages, newMessageText]);
+    socket.send(newMessageText);
     setNewMessageText('');
   }
+
+  useEffect(() => {
+    setUpWebSocket(newMessage => {
+      setMessages([...messages, newMessage]);
+    });
+  }, [messages]);
 
   return (
     <>
